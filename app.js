d52e0d74bbd7187bdcaf4fc743046d61a850b0dd
Melisa Lachev <melisatormac55@gmail.com>
2025-04-26 01:42:09 +0300

    made it so  number box shows guessed number

--- app.js
+++ app.js
@@ -1,54 +1,51 @@
-const replayButton = document.querySelector(`.again-btn`)
-const checkButton = document.querySelector(`.check-btn`)
+const replayButton = document.querySelector(`.again-btn`);
+const checkButton = document.querySelector(`.check-btn`);
 
-let numberBox = document.querySelector(`.number`)
-let warningText = document.querySelector(`.warning-box`)
-let score = document.querySelector(`.score-point`)
-let highScore = document.querySelector(`.hs-point`)
+let numberBox = document.querySelector(`.number`);
+let warningText = document.querySelector(`.warning-box`);
+let score = document.querySelector(`.score-point`);
+let highScore = document.querySelector(`.hs-point`);
 const scoresArray = [];
 
-let guessNumber = document.querySelector(`.guess`)
+let guessNumber = document.querySelector(`.guess`);
 
-let correctNumber = Math.floor(Math.random() * 20+1);
+let correctNumber = Math.floor(Math.random() * 20 + 1);
 
 const checkNumber = function (num) {
-    console.log(correctNumber)
-    if (num < correctNumber) {
-        warningText.textContent = `Too low!`
-        score.textContent--
-    } else if (num > correctNumber) {
-        warningText.textContent = `Too high!`
-        score.textContent--
-    } else {
-        warningText.textContent = `Correct!`
-        correctNumber = Math.floor(Math.random() * 20 +1);
-        scoresArray.push(score.textContent)
-        console.log(scoresArray)
-        score.textContent=20
-    }
-}
+  console.log(correctNumber);
+  if (num < correctNumber) {
+    warningText.textContent = `Too low!`;
+    score.textContent--;
+  } else if (num > correctNumber) {
+    warningText.textContent = `Too high!`;
+    score.textContent--;
+  } else {
+    warningText.textContent = `Correct!`;
+    correctNumber = Math.floor(Math.random() * 20 + 1);
+    scoresArray.push(score.textContent);
+    score.textContent = 20;
+  }
+};
 
-const findHighScore = function (scoreArr){
-    let maxScore = 0;
-    for (let i = 0; i <= scoreArr.length; i++){
+const findHighScore = function (scoreArr) {
+  let maxScore = 0;
+  for (let i = 0; i <= scoreArr.length; i++) {
     if (maxScore < scoreArr[i]) maxScore = scoreArr[i];
-    }
-    highScore.textContent = maxScore;
-}
-
+  }
+  highScore.textContent = maxScore;
+};
 
 checkButton.addEventListener(`click`, () => {
-    checkNumber(guessNumber.value)
-    findHighScore(scoresArray)
-    console.log(correctNumber)
-})
+  checkNumber(guessNumber.value);
+  findHighScore(scoresArray);
+  numberBox.textContent = `${guessNumber.value}`;
+});
 
 replayButton.addEventListener(`click`, () => {
-    score.textContent=20
-    warningText.textContent=`Start guessing!`
-    highScore.textContent=``
-    guessNumber.value =``
-    scoresArray = [];
-    correctNumber = Math.floor(Math.random() * 20 +1);
-    console.log(correctNumber)
-})
+  score.textContent = 20;
+  warningText.textContent = `Start guessing!`;
+  highScore.textContent = ``;
+  guessNumber.value = ``;
+  scoresArray = [];
+  correctNumber = Math.floor(Math.random() * 20 + 1);
+});
